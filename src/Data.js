983651a0302d953b6b0e36ecee2ed7d823ffe67a
Matthew Hons <devwithmatt@gmail.com>
2022-04-12 18:26:20 +0200

    fix bug with react 18 -> Root

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 function Data() {
-  const [input, setInput] = useState(""); // '' is the initial state value
+  const [input, setInput] = useState("");
+  const NameRef = useRef(); // '' is the initial state value
   let API_KEY = process.env.REACT_APP_API_KEY;
   let API_KEY_SPLASH = process.env.REACT_APP_API_KEY1;
   const fetchData = async () => {
@@ -16,13 +17,23 @@ function Data() {
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
   };
-
+  function handleAddData(e) {
+    if (e.keyCode === 13) {
+      const name = NameRef.current.value;
+      NameRef.current.value = null;
+      fetchData();
+    }
+  }
   return (
     <div>
-      <p>Hello world</p>
       <label>Please specify:</label>
-      <input value={input} onInput={(e) => setInput(e.target.value)} />
-      <button onClick={fetchData}>Click</button>
+      <input
+        onKeyDown={handleAddData}
+        ref={NameRef}
+        value={input}
+        onInput={(e) => setInput(e.target.value)}
+      />
+      <button onClick={fetchData}>Submit</button>
     </div>
   );
 }
