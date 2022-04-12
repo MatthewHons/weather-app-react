bacee0fd8ecf4ac678acfbb466781c0e16670b3a
Matthew Hons <devwithmatt@gmail.com>
2022-04-12 18:06:01 +0200

    fetch data from unsplash api

--- src/Data.js
+++ src/Data.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 function Data() {
   const [input, setInput] = useState(""); // '' is the initial state value
   let API_KEY = process.env.REACT_APP_API_KEY;
-
+  let API_KEY_SPLASH = process.env.REACT_APP_API_KEY1;
   const fetchData = async () => {
     await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${input}&lang=fr&units=metric&appid=${API_KEY}`
@@ -11,10 +11,10 @@ function Data() {
       .then((result) => console.log(result))
       .catch((error) => console.log("error", error));
 
-    // await fetch(`https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY`)
-    //   .then((response) => response.text())
-    //   .then((result) => console.log(result))
-    //   .catch((error) => console.log("error", error));
+    await fetch(`https://api.unsplash.com/photos/?client_id=${API_KEY_SPLASH}`)
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log("error", error));
   };
 
   return (
